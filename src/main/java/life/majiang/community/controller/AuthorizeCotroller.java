105d1ae39d92616587a1f3ee99783b6fc079fac3
fwt <fwt@163.com>
2019-11-28 15:41:53 +0800

    git session

--- src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
+++ src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeCotroller {
 
@@ -24,7 +26,8 @@ public class AuthorizeCotroller {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTD accessTokenDTD = new AccessTokenDTD();
         accessTokenDTD.setCode(code);
         accessTokenDTD.setRedirect_uri(redirectUri);
@@ -33,7 +36,15 @@ public class AuthorizeCotroller {
         accessTokenDTD.setClient_id(clientID);
         String accessToken = githubProvider.getAccessToken(accessTokenDTD);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        //System.out.println(user.getName());
+        if(user != null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+
     }
 }
