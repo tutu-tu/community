4db3a8ff8540ad8363a72ad93557aa6a774bd1f3
fwt <fwt@163.com>
2019-11-28 12:41:03 +0800

    changes to the application.properties

--- src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
+++ src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
@@ -4,6 +4,7 @@ import life.majiang.community.entity.AccessTokenDTD;
 import life.majiang.community.entity.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,22 @@ public class AuthorizeCotroller {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.id}")
+    private String clientID;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTD accessTokenDTD = new AccessTokenDTD();
         accessTokenDTD.setCode(code);
-        accessTokenDTD.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTD.setRedirect_uri(redirectUri);
         accessTokenDTD.setState(state);
-        accessTokenDTD.setClient_secret("fdabf177dea0caf4a19aa707a1a7dee7b5b07103");
-        accessTokenDTD.setClient_id("572f119c724905b046f1");
+        accessTokenDTD.setClient_secret(clientSecret);
+        accessTokenDTD.setClient_id(clientID);
         String accessToken = githubProvider.getAccessToken(accessTokenDTD);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
